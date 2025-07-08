02d0754b0dce0c17cce01fc2e2b1ea487294e85d
Sachithra Indrachapa <sachithrai1003@gmail.com>
2025-07-08 22:18:51 +0530

    add NotFound page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { NotFound } from './pages/NotFound'; 
+
 
 function App() {
 
   return (
     <>
      <BrowserRouter>
-        <Route index element={/>
+      <Routes>
+        <Route index element={<Home />}/>
+        <Route path="*" element = {<NotFound />}/>
+      </Routes>
      </BrowserRouter>
     </>
   )
